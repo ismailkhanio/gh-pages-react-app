be9bfdd0a43cd05dfeb9d27c4568eb7104ac2690
Ismail Khan <ismailislamkhan@gmail.com>
2020-09-28 12:34:42 -0400

    Updates

---
+++ precache-manifest.d15c213f22a214f47e8e3939ebec3236.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "456837f399ed9086685032135d0ef93f",
+    "url": "/ismailkhanio/gh-pages-react-app.git/index.html"
+  },
+  {
+    "revision": "5fc35ab518b17b905ed0",
+    "url": "/ismailkhanio/gh-pages-react-app.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7a02fc9105f64e369914",
+    "url": "/ismailkhanio/gh-pages-react-app.git/static/js/2.d6f0c39a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ismailkhanio/gh-pages-react-app.git/static/js/2.d6f0c39a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc35ab518b17b905ed0",
+    "url": "/ismailkhanio/gh-pages-react-app.git/static/js/main.23345ce0.chunk.js"
+  },
+  {
+    "revision": "41b2f9becba2e99a5dad",
+    "url": "/ismailkhanio/gh-pages-react-app.git/static/js/runtime-main.3138821d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ismailkhanio/gh-pages-react-app.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
